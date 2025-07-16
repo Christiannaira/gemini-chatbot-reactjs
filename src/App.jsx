ebc98d90e291288fc1b63a36f6cf4d7682786f79
Christian <Christianmnaira@gmail.com>
2025-07-16 13:12:16 +0800

    feat: initialized theme interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,42 @@ import { Menu } from "lucide-react";
 
 function App() {
 
-  return (
-    <>
-    hello world
-    </>
-  )
+   const[isLoading, setIsLoading] = useState(false);
+   const typingInterval = useRef(null);
+   const messagesContainerRef = useRef(null);
+   const [isSidebarOpen, setIsSidebarOpen] = useState(() => window.innerWidth > 768);
+
+   const [theme, setTheme] = useState(() => {
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme) {
+         return savedTheme;
+      }
+
+      const prefersDark = window.matchMedia(
+         "(prefers-color-scheme: dark)"
+      ).matches;
+      return prefersDark ? "dark" : "light";
+   });
+
+
+
+   return (
+      <div
+         className={`app-container ${
+            theme === "light" ? "light-theme" : "dark-theme"
+         }`}
+      >
+
+         <main className="main-container">
+            <header className="main-header">
+              <button className="sidebar-toggle">
+                <Menu size={18} />
+              </button>
+            </header>
+         </main>
+
+      </div>
+   );
 }
 
-export default App
+export default App;
